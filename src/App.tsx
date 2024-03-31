d194f65bc32da00110ed62fcf16f57e97b6765c8
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-04-01 01:26:21 +0530

    🎯Created mint token

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import MintToken from './components/MintToken';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hi</p>
+        <MintToken/>
       </header>
     </div>
   );
