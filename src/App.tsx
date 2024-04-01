dcc9e731caf82fb92d606fc889571a0cb9343f87
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-04-01 20:14:29 +0530

    🔨Added navbar with connection to wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import './App.css';
-import MintNft from './components/MintNft';
-import MintToken from './components/MintToken';
+import "./App.css";
+import Header from "./components/Header";
+import MintNft from "./components/MintNft";
+import MintToken from "./components/MintToken";
+import { useMemo } from "react"; //react hook
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 function App() {
+  const endpoint =
+    "https://fluent-little-sun.solana-devnet.quiknode.pro/371c43da2c0c0ea39a2d68d940b83d1c7a0a05b5/";
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <MintToken/>
-        <MintNft/>
-      </header>
-    </div>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <Header />
+          <div className="App">
+            <header className="App-header">
+              <MintToken />
+              <MintNft />
+            </header>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import style from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.title}>NFT Minting App </div>
+      <WalletMultiButton/>
+    </div>
+  );
+};
+
+export default Header;
